0f9b81c7adb4fc11c1fc74ff37dc6a29fce67888
julia-kv <ulya_kor97@yandex.ru>
2021-10-14 00:19:13 +0300

    New style

--- src/main.c
+++ src/main.c
@@ -1,41 +1,42 @@
+#include "../include/schedule.h"
 #include "stdio.h"
 #include "stdlib.h"
-#include "../include/schedule.h"
 
 int main() {
-    printf("Создайте расписание поездов\n");
-    printf("\n");
+  printf("Создайте расписание поездов\n");
+  printf("\n");
 
-    struct schedule new_schedule;
+  struct schedule new_schedule;
 
-    printf("Введите количество поездов в расписании: ");
-    int new_trains_number = 0;
-    scanf("%d", &new_trains_number);
-    printf("\n");
+  printf("Введите количество поездов в расписании: ");
+  int new_trains_number = 0;
+  scanf("%d", &new_trains_number);
+  printf("\n");
 
-    if (inputSchedule(&new_schedule, new_trains_number) == 0) {
-        printf("Расписание не создалось\n");
+  if (inputSchedule(&new_schedule, new_trains_number) == 0) {
+    printf("Расписание не создалось\n");
 
-    } else {
-        printf("Поиск времения отправления ближайшего поезда\n");
-        printf("Введите название ближайшей станции: ");
-        char *current_station;
-        current_station = (char *) malloc(10);
-        scanf("%9s", current_station);
+  } else {
+    printf("Поиск времения отправления ближайшего поезда\n");
+    printf("Введите название ближайшей станции: ");
+    char* current_station;
+    current_station = (char*)malloc(10);
+    scanf("%9s", current_station);
 
-        printf("Введите название станции, до которой нужно добраться: ");
-        char *goal_station;
-        goal_station = (char *) malloc(10);
-        scanf("%9s", goal_station);
+    printf("Введите название станции, до которой нужно добраться: ");
+    char* goal_station;
+    goal_station = (char*)malloc(10);
+    scanf("%9s", goal_station);
 
-        printf("Введите время формате часы:минуты : ");
-        int new_hour = 0;
-        int new_min = 0;
-        scanf("%d%d", &new_hour, &new_min);
+    printf("Введите время формате часы:минуты : ");
+    int new_hour = 0;
+    int new_min = 0;
+    scanf("%d%d", &new_hour, &new_min);
 
-        if (findTime(&new_schedule, current_station, goal_station, new_hour, new_min) == 0) {
-            return 0;
-        }
+    if (findTime(&new_schedule, current_station, goal_station, new_hour,
+                 new_min) == 0) {
+      return 0;
     }
-    return 1;
+  }
+  return 1;
 }
